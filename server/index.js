5bb4d68f99a73bf3ae90244dd6d2614ecff28e61
Harsh D. Rao <harshrao26>
2025-05-20 12:50:13 +0530

    ...

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ import affliateRouter from './route/affiliate.Routes.js'
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://e-commerce-one-orpin-93.vercel.app', 'http://sparknstitch.com','https://www.sparknstitch.com/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://e-commerce-one-orpin-93.vercel.app', 'http://sparknstitch.com','https://www.sparknstitch.com', 'https://www.sparknstitch.com/'],
     credentials: true
   }));
 app.options('*', cors())
